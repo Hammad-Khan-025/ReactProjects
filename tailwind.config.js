fb2a6a82de760efb37b939939f5567475a53b3ac
Hammad Khan <hk188540@gmail.com>
2024-08-01 14:39:12 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow:{
+        "custom": "0 0 10px #26A69A"
+      }
+    },
+  },
+  plugins: [],
+}
+
